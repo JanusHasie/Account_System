ac13096920041c5ebdb0357b8162c71e9418382b
JanusHasie <janushaasbroek@gmail.com>
2021-10-05 16:09:28 +0200

    Finally my Initial commit

---
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/config/WebConfig.java
@@ -0,0 +1,7 @@
+package za.ac.nwu.ac.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class WebConfig {
+}
