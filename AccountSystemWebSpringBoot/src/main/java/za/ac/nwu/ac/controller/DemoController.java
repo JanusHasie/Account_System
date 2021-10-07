363621d9a2f72e9324a8315b8a0b4f9f527dc11c
JanusHasie <janushaasbroek@gmail.com>
2021-10-07 17:04:19 +0200

    commit message

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/controller/DemoController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/controller/DemoController.java
@@ -1,4 +1,4 @@
-package za.ac.nwu.ac.controller;
+//package za.ac.nwu.ac.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
